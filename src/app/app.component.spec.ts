94b118d5371b228ee47e96dfbd091d95b4b5de0d
AkashGP2 <vakash@deloitte.com>
2023-01-10 00:43:29 +0530

    specs.ts modified

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'Jas-Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Jas-Angular');
+    expect(app.title).toEqual('JAS-ANGULAR');
   });
 
   it('should render title', () => {
